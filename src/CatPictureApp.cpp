43cf5a2ef7763fb2fa5962a5f73dafd797b6f86b
Eddie Obissi <obissiam@muohio.edu>
2012-09-07 11:32:44 -0400

    Made some changes to the code.
    Got the copy function working and changed some syntax. Also added
    comments where necessary.

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -18,19 +18,48 @@ class CatPictureApp : public AppBasic {
 	void update();
 	void draw();
 	void prepareSettings(Settings* settings);
-	
+
 
 	private:
-		//float currentColor;
-		void createLine(uint8_t* pixles, int startX, int startY, int segLength);
+		/*///////////////////////////////////////////////////////////
+		* I removed your currentColor comment. It seemed unnecessary.
+		* However, we should add some description to each of these
+		* functions as we initialize them. I'm moving your 
+		* descriptions from the body of your program up here*/
+		
+		/*///////////////////////////////////////////////////////////
+		* I also arranged your functions so they would be in the same 
+		* order here and in the body of your program */
+
+		/* Draws a rectangle for requirement A 1, like the steak sauce */
+		/*//////////////////////////////////////////////////////////////////////////
+		* Fills the rectangle with a predetermined color */
 		void makeRect(uint8_t* pixles, int startX, int startY, int width, int height);
-		void makeCopy(uint8_t* pixles, int startX, int startY, int width, int height, int x2, int y2);
+
+		/* Draws a circle for requirement A 2 */
+		/*//////////////////////////////////////////////////////////////////////////
+		* Fills the circle with a predetermined color */
 		void makeCircle(uint8_t* pixles, int x, int y, int radius);
+
+		/* Draws a line that looks kinda like a green glowstick for requirement A 3 */
+		void createLine(uint8_t* pixles, int startX, int startY, int segLength);
+
+		/* Copies an area of pixles for requirement A 5 */
+		void makeCopy(uint8_t* pixles, int startX, int startY, int width, int height, int x2, int y2);
+
+		/* Blurs the image for requirement B 1 */
 		void blur(uint8_t* pixles);
 		Surface* mySurface;
+
+		/*/////////////////////////////////////
+		Added a description of your variables*/
+
+		// Window dimensions
 		static const int windowWidth=800;
 		static const int windowHeight=600;
+		// Number of pixels
 		static const int surfaceSize=1024;
+		// The frame number
 		int updateCount;
 };
 
@@ -40,9 +69,104 @@ void CatPictureApp::prepareSettings(Settings* settings)
 	(*settings).setResizable(false);
 }
 
-/*
-Blurs the image for requirement B 1
-*/
+void CatPictureApp::makeRect(uint8_t* pixles, int startX, int startY, int width, int height)
+{
+	/*///////////////////////////////////////////////////////
+	* i, j, k is usually an alternate way of referring to the 
+	* x, y, z coordinate axes. I switched i and j so that
+	* j is matched up with y and i is matched with x.*/
+
+	/*/////////////////////////////////////////////////
+	* You don't end up using these
+	int x=startX;
+	int y=startY;
+	*/
+	int offset;
+
+	/*/////////////////////////////////////////////////////
+	*Switched ++j to j++ so the addition happens after the
+	*iteration. I don't think it makes a difference here.
+	*But this is usually the way to go. */
+	for(int j=startY; j<height+startY; j++)
+	{
+		for(int i=startX; i<width+startX; i++)
+		{
+			/*////////////////////////////////////////////////////////////////
+			* There's nothing wrong with making every rectangle the same color,
+			* but the user should be aware of this. Add it to the description */
+
+			offset=3*(i+j*surfaceSize);
+			pixles[offset]=100;
+			pixles[offset+1]=150;
+			pixles[offset+2]=175;
+		}
+	}
+}
+
+void CatPictureApp::makeCircle(uint8_t* pixles, int x, int y, int radius)
+{
+		if (radius <= 0) return;
+
+		for(int newY = y-radius; newY <= y+radius; newY++) {
+			for (int newX=x-radius; newX <= x+radius; newX++) {
+
+				int newR = (int)sqrt((double)((newX-x)*(newX-x) + (newY-y)*(newY-y)));
+
+				if(newR <= radius) {
+				int offset = 3*(newX+newY*surfaceSize);
+				pixles [offset] = 50;
+				pixles [offset+1] = 100;
+				pixles [offset+2] =255;
+				}
+			}
+		}
+}
+
+void CatPictureApp::createLine(uint8_t* pixles, int startX, int startY, int segLength)
+{
+	int x=startX;
+	int y=startY;
+	for(int i=0; i< segLength; ++i)
+	{
+		pixles[3*(x+y*surfaceSize)]=0;
+		pixles[3*(x+y*surfaceSize)+1]=200;
+		pixles[3*(x+y*surfaceSize)+2]=0;
+		++x;
+		++y;
+	}
+}
+
+void CatPictureApp::makeCopy(uint8_t* pixles, int startX, int startY, int width, int height, int x2, int y2)
+{
+	/*//////////////////////////////////////////////////////////////////////
+	* I changed the i's j's and the ++'s again */
+
+	/*//////////////////////////////
+	* You don't end up using these
+	int x1=startX;
+	int y1=startY;
+	*/
+	int offset;
+	int origX2=x2;
+	for(int i=startY; i<height+startY; ++i)
+	{
+		for(int j=startX; j<width+startX; ++j)
+		{
+			/*///////////////////////////////////////////////
+			*Your copy function wasn't working correctly
+			*because you forgot to multiply the address 
+			*the left by 3. I did that for you. Now it works.*/
+			offset=3*(j+i*surfaceSize);
+			pixles[3*(x2+y2*surfaceSize)]=pixles[offset];
+			pixles[3*(x2+y2*surfaceSize)+1]=pixles[offset+1];
+			pixles[3*(x2+y2*surfaceSize)+2]=pixles[offset+2];
+			++x2;
+		}
+		++y2;
+		x2=origX2;
+	}
+}
+
 void CatPictureApp::blur(uint8_t* pixles)
 {
 	int offset;
@@ -71,95 +195,10 @@ void CatPictureApp::blur(uint8_t* pixles)
 	}
 }
 
-
-/*
-Draws a circle for requirement A 2
-*/
-void CatPictureApp::makeCircle(uint8_t* pixles, int x, int y, int radius)
-{
-		if (radius <= 0) return;
-
-		for(int newY = y-radius; newY <= y+radius; newY++) {
-			for (int newX=x-radius; newX <= x+radius; newX++) {
-
-				int newR = (int)sqrt((double)((newX-x)*(newX-x) + (newY-y)*(newY-y)));
-
-				if(newR <= radius) {
-				int offset = 3*(newX+newY*surfaceSize);
-				pixles [offset] = 50;
-				pixles [offset+1] = 100;
-				pixles [offset+2] =255;
-				}
-			}
-		}
-}
-
-
-/*
-Copies an area of pixles for requirement A 5
-*/
-void CatPictureApp::makeCopy(uint8_t* pixles, int startX, int startY, int width, int height, int x2, int y2)
-{
-	int x1=startX;
-	int y1=startY;
-	int offset;
-	int origX2=x2;
-	for(int i=startY; i<height+startY; ++i)
-	{
-		for(int j=startX; j<width+startX; ++j)
-		{
-			offset=3*(j+i*surfaceSize);
-			pixles[x2+y2*surfaceSize]=pixles[offset];
-			pixles[x2+y2*surfaceSize+1]=pixles[offset+1];
-			pixles[x2+y2*surfaceSize+2]=pixles[offset+2];
-			++x2;
-		}
-		++y2;
-		x2=origX2;
-	}
-}
-
-/*
-Draws a rectangle for requirement A 1, like the steak sauce
-*/
-void CatPictureApp::makeRect(uint8_t* pixles, int startX, int startY, int width, int height)
-{
-	int x=startX;
-	int y=startY;
-	int offset;
-	for(int i=startY; i<height+startY; ++i)
-	{
-		for(int j=startX; j<width+startX; ++j)
-		{
-			offset=3*(j+i*surfaceSize);
-			pixles[offset]=100;
-			pixles[offset+1]=150;
-			pixles[offset+2]=175;
-		}
-	}
-}
-/*
-Draws a line that looks kinda like a green glowstick for requirement A 3
-*/
-void CatPictureApp::createLine(uint8_t* pixles, int startX, int startY, int segLength)
-{
-	int x=startX;
-	int y=startY;
-	for(int i=0; i< segLength; ++i)
-	{
-		pixles[3*(x+y*surfaceSize)]=0;
-		pixles[3*(x+y*surfaceSize)+1]=200;
-		pixles[3*(x+y*surfaceSize)+2]=0;
-		++x;
-		++y;
-	}
-}
-
 void CatPictureApp::setup()
 {
-	//currentColor=0.0f;
 	updateCount=0;
-	mySurface=new Surface(surfaceSize, surfaceSize, false);
+	mySurface= new Surface(surfaceSize, surfaceSize, false);
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -179,10 +218,10 @@ void CatPictureApp::update()
 	{
 		currentColor=0.0f;
 	}*/
-	createLine(pixels, 20, 200, 100);
+	//createLine(pixels, 20, 200, 100);
 	makeRect(pixels, 10, 40, 100, 150);
 	makeCopy(pixels, 10, 40, 100, 150, 200, 40);
-	makeCircle(pixels, 120, 220, 25);
+	//makeCircle(pixels, 120, 220, 25);
 	if(updateCount==26)
 	{
 		writeImage("works.png", *mySurface);// kinda looks like a mouse
@@ -191,8 +230,11 @@ void CatPictureApp::update()
 
 void CatPictureApp::draw()
 {
+	/*////////////////////////////////////////////
+	* We don't need the clear. I got rid of it.
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) );
+	*/
 	gl::draw(*mySurface);
 }
 
